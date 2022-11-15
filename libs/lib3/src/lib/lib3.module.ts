092e4a9c79f132f9df37cb7c04b8cfa9c55c04d3
InnovativeUS <86477631+InnovativeUS@users.noreply.github.com>
2022-11-15 11:56:28 +0000

    feat(*): add lib4 and reference in lib3

--- libs/lib3/src/lib/lib3.module.ts
+++ libs/lib3/src/lib/lib3.module.ts
@@ -2,13 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ButtonComponent } from './button/button.component';
 
+import { Lib4Module } from '@angular-tailwind-nx/lib4';
+
 @NgModule({
-  imports: [CommonModule],
-  declarations: [
-    ButtonComponent
-  ],
-  exports: [
-    ButtonComponent
-  ],
+  imports: [CommonModule, Lib4Module],
+  declarations: [ButtonComponent],
+  exports: [ButtonComponent],
 })
 export class Lib3Module {}
